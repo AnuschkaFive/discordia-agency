2d9a6d5a98179d7156c7ab8357b97c4c4ca7141b
Dennis Maas <dennis.ma@live.de>
2017-07-20 18:51:47 +0200

    Texturen + Bewegung - keine Kollision oder Limitationen.

---
+++ Discordia Agency/Assets/Sprites/EntityBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityBase : MonoBehaviour {
+    const int fieldHeight = 9;
+    const int fieldWidth = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected void ResizeRelativeToField()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 xWidth = transform.localScale;
+        xWidth.x = worldScreenWidth / width;
+
+        Vector3 yHeight = transform.localScale;
+        yHeight.y = worldScreenHeight / height;
+
+        Vector3 scale = new Vector3(1, 1, 1);
+        scale.x = ((worldScreenHeight > worldScreenWidth) ? worldScreenWidth / width : worldScreenHeight / height) / fieldWidth;
+        scale.y = ((worldScreenHeight > worldScreenWidth) ? worldScreenWidth / width : worldScreenHeight / height) / fieldHeight;
+
+        transform.localScale = scale;
+    }
+}
---
+++ Discordia Agency/Assets/Sprites/Ground.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Resize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Resize()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 xWidth = transform.localScale;
+        xWidth.x = worldScreenWidth / width;
+
+        Vector3 yHeight = transform.localScale;
+        yHeight.y = worldScreenHeight / height;
+
+        Vector3 scale = new Vector3(1, 1, 1);
+        scale.x = (worldScreenHeight > worldScreenWidth) ? worldScreenWidth / width : worldScreenHeight / height;
+        scale.y = scale.x;
+
+        transform.localScale = scale;
+    }
+}
