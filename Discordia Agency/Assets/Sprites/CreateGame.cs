bbf97affadc4c7c1618f81fa67d9a8d0c915b2bb
Dennis Maas <dennis.ma@live.de>
2017-07-27 14:03:57 +0200

    Files hinzugefuegt

---
+++ Discordia Agency/Assets/Sprites/CreateGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class CreateGame : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void loadGame(string lvlName) {
+        XmlReader reader = XmlReader.Create("Level1.xml");
+
+        while (reader.Read()) {
+
+        }
+
+        reader.ReadEndElement();
+        reader.Close();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
