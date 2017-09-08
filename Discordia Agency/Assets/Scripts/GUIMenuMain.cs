f2f06b6cd10939838b103beb35fd8756769a584c
Anna Franziska Horne <afhorne@web.de>
2017-09-08 11:45:17 +0200

    Start-Menu added

---
+++ Discordia Agency/Assets/Scripts/GUIMenuMain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIMenuMain : MonoBehaviour {
+
+    public Button start;
+    public Button exit;
+
+    void Start()
+    {
+        start.GetComponent<Button>().onClick.AddListener(ClickStart);
+        exit.GetComponent<Button>().onClick.AddListener(ClickExit);
+    }
+
+    void ClickStart()
+    {
+        Debug.Log("Level startet!");
+        SceneManager.LoadScene("Level01");
+    }
+
+    void ClickExit()
+    {
+        Debug.Log("Spiel beendet sich");
+        Application.Quit();
+    }
+}
